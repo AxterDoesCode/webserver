372200dd92cbe44870022b472f91574bef8797ff
AxterDoesCode <chau.alex88@hotmail.com>
2023-06-09 14:54:15 +0100

    Admin namespace added

--- main.go
+++ main.go
@@ -1,62 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	const port = "8080"
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
+	apiRouter := chi.NewRouter()
+	adminRouter := chi.NewRouter()
+
 	apiCfg := apiConfig{fileserverHits: 0}
-	mux.Handle(
-		"/app/",
+	r.Handle(
+		"/app/*",
 		apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))),
 	)
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.showMetrics)
-	corsMux := middlewareCors(mux)
+	r.Handle(
+		"/app",
+		apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))),
+	)
+	r.Mount("/api", apiRouter)
+	r.Mount("/admin", adminRouter)
+
+	apiRouter.Get("/healthz", handlerReadiness)
+	adminRouter.Get("/metrics", apiCfg.handlerMetrics)
+	corsr := middlewareCors(r)
 
 	server := &http.Server{
 		Addr:    ":" + port,
-		Handler: corsMux,
+		Handler: corsr,
 	}
 
 	log.Printf("Serving on port : %s\n", port)
 	log.Fatal(server.ListenAndServe())
 }
 
-type apiConfig struct {
-	fileserverHits int
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits += 1
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) showMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
-}
-
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content=Type", "text-plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
 }
